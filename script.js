217ae51a5cc8d11b918d8938ad4d797b9f660042
ReUN9 <90161391+Alexander-Metzger@users.noreply.github.com>
2024-07-02 10:45:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('download-btn').addEventListener('click', function() {
     const link = document.createElement('a');
     
-    link.href = '/home/alex/Code/metzger-studio/manifest.pdf';  // Replace with the actual path to your manifest file
+    link.href = 'metzger-studio/manifest.pdf';  
     
     link.download = 'manifest.pdf';
     
